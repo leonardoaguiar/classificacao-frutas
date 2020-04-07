fa0139a6d1ea601823fe52859f9796327924e51c
Leonardo Ribeiro de Aguiar <leonardo.aguiar@itst.com.br>
2020-04-07 15:56:02 -0300

    criando o meu classificador baseado no KNeighbors Classifier

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,38 @@ from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
+
+from scipy.spatial import distance
+
+
+def euc(a, b):
+    return distance.euclidean(a, b)
+
+
+class MyKNeighborsClassifier():
+    def fit(self, x_train, y_train):
+        self.x_train = x_train
+        self.y_train = y_train
+
+    def predict(self, x_tests):
+        predictions = []
+        for row in x_tests:
+            label = self.closest(row)
+            predictions.append(label)
+        return predictions
+
+    def closest(self, row):
+        best_distance = euc(row, self.x_train[0])
+        best_index = 0
+        for i in range(1, len(self.x_train)):
+            dist = euc(row, self.x_train[i])
+            if dist < best_distance:
+                best_distance = dist
+                best_index = i
+
+        return self.y_train[best_index]
+
+
 iris = datasets.load_iris()
 
 x = iris.data  # dados de entreda para o algoritmo de classificação
@@ -24,7 +56,8 @@ print("x_test size: ", x_test.size)
 print("y_train size: ", y_train.size)
 print("y_test size: ", y_test.size)
 
-classifier = KNeighborsClassifier()
+# classifier = KNeighborsClassifier()
+classifier = MyKNeighborsClassifier()
 
 classifier.fit(x_train, y_train)
 
