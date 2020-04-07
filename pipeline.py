3104ee6fb80c433d6070195f5d963de3d6a03731
Leonardo Ribeiro de Aguiar <leonardo.aguiar@itst.com.br>
2020-04-07 15:59:27 -0300

    importando o modulo MyKNeighborsClassifier para simplificar o pipeline

--- pipeline.py
+++ pipeline.py
@@ -2,37 +2,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
-
-from scipy.spatial import distance
-
-
-def euc(a, b):
-    return distance.euclidean(a, b)
-
-
-class MyKNeighborsClassifier():
-    def fit(self, x_train, y_train):
-        self.x_train = x_train
-        self.y_train = y_train
-
-    def predict(self, x_tests):
-        predictions = []
-        for row in x_tests:
-            label = self.closest(row)
-            predictions.append(label)
-        return predictions
-
-    def closest(self, row):
-        best_distance = euc(row, self.x_train[0])
-        best_index = 0
-        for i in range(1, len(self.x_train)):
-            dist = euc(row, self.x_train[i])
-            if dist < best_distance:
-                best_distance = dist
-                best_index = i
-
-        return self.y_train[best_index]
-
+from meu_kneighbors_classifier import MyKNeighborsClassifier
 
 iris = datasets.load_iris()
 
