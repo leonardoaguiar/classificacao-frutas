f675938459d1878641d222fde2ce07fe56449c65
Leonardo Ribeiro de Aguiar <leonardo.aguiar@itst.com.br>
2020-04-07 16:05:53 -0300

    renomeando arquivos python

--- pipeline.py
+++ pipeline.py
@@ -2,7 +2,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
-from meu_kneighbors_classifier import MyKNeighborsClassifier
+from myKNeighborsClassifier import MyKNeighborsClassifier
 
 iris = datasets.load_iris()
 
