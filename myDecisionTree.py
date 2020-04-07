8c02d94a5b295df2cb91c16a70ccb076bdabe8c0
Leonardo Ribeiro de Aguiar <leonardo.aguiar@itst.com.br>
2020-04-07 18:41:27 -0300

    definição do nomes das colunas e criação da função unique_vals

--- myDecisionTree.py
+++ myDecisionTree.py
@@ -10,3 +10,18 @@ training_data = [
     ['Vermelho', 1, 'Uva'],
     ['Amarelo', 3, 'Limão'],
 ]
+
+# Nome das colunas.
+# somente para exibir a árvore.
+header = ["cor", "diâmetro", "fruta"]
+
+
+def unique_vals(rows, col):
+    """Encontra um valor único em cada coluna na tabela de exemplos."""
+    return set([row[col] for row in rows])
+
+
+#######
+# print(unique_vals(training_data, 0)) # identifica os valores da primeira coluna
+# print(unique_vals(training_data, 1)) # identifica os valores da segunda coluna
+#######
